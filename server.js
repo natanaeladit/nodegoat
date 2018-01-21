4bccb5b0c3730aee51199aa0db4c6a1a6946e1bd
Natanael <nata.adit@gmail.com>
2018-01-21 12:19:18 +0700

    Fix XSS flaw

--- server.js
+++ server.js
@@ -135,11 +135,11 @@ MongoClient.connect(config.db, function(err, db) {
     // Template system setup
     swig.setDefaults({
         // Autoescape disabled
-        autoescape: false
-        /*
-        // Fix for A3 - XSS, enable auto escaping
+        // autoescape: false
+        
+        // Fix for A7 - XSS, enable auto escaping
         autoescape: true // default value
-        */
+        
     });
 
 	/*
